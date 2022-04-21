02c40504ae7aaf34c91359306dfee8608412af0b
김성은 <kse940815@naver.com>
2022-04-21 19:44:00 +0900

    비밀키 숨김

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nzs6)f8wvff%17*g-t3!-mw@kkb0$0dd6t1!t1j9^smhrye@9q'
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
