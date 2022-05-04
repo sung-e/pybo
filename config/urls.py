ddf2c0960dc1aebebfb9cf65630f0851af29e71b
김성은 <kse940815@gmail.com>
2022-05-04 23:58:38 +0900

    3-9 답변 수정 폼작성중

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from pybo import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls'))
